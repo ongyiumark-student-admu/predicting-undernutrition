ee8b1c58356f0139d81c1edd43906d35aeb7682b
Mark Kevin A. Ong Yiu <markkevin.ongyiu@gmail.com>
2022-09-25 22:57:59 +0800

    edited main.py

--- code/main.py
+++ code/main.py
@@ -8,8 +8,8 @@ TASK = '2aii'
 
 if __name__ == '__main__':
     train = pd.read_csv(os.path.join(DATA_DIR, f'{TASK}_train.csv'), index_col=0)
-    results = puh.tune_dnn(train, TASK, 10, False)
+    results = puh.tune_dnn(train, TASK, 10, 17, False)
     results.to_csv(os.path.join(RESULTS_DIR, 'preliminary_dnn.csv'), index=False)
 
-    results = puh.tune_dnn(train, TASK, 10, True)
+    results = puh.tune_dnn(train, TASK, 10, 17, True)
     results.to_csv(os.path.join(RESULTS_DIR, 'preliminary_dnn_sm.csv'), index=False)
