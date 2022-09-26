f3f9c83b2a6bfc7cd4dbd77b451064af4a1b4187
Mark Kevin A. Ong Yiu <markkevin.ongyiu@gmail.com>
2022-09-26 16:39:04 +0800

    added spacing in tune dnn for readability

--- code/main.py
+++ code/main.py
@@ -6,10 +6,22 @@ DATA_DIR = '../train-test-data'
 RESULTS_DIR = '../results'
 TASK = '2aii'
 
-if __name__ == '__main__':
+
+def run_dnns():
     train = pd.read_csv(os.path.join(DATA_DIR, f'{TASK}_train.csv'), index_col=0)
-    results = puh.tune_dnn(train, TASK, 10, 17, "adasyn")
+    n = len(train.columns)-1
+    results = puh.tune_dnn(train, TASK, 10, n)
+    results.to_csv(os.path.join(RESULTS_DIR, 'preliminary_dnn.csv'), index=False)
+
+    results = puh.tune_dnn(train, TASK, 10, n, "smote")
+    results.to_csv(os.path.join(RESULTS_DIR, 'preliminary_dnn_sm.csv'), index=False)
+
+    results = puh.tune_dnn(train, TASK, 10, n, "adasyn")
     results.to_csv(os.path.join(RESULTS_DIR, 'preliminary_dnn_adasyn.csv'), index=False)
 
-    results = puh.tune_dnn(train, TASK, 10, 17, "borderline")
+    results = puh.tune_dnn(train, TASK, 10, n, "borderline")
     results.to_csv(os.path.join(RESULTS_DIR, 'preliminary_dnn_borderline.csv'), index=False)
+
+
+if __name__ == '__main__':
+    run_dnns()
--- src/predunder/hypertuning.py
+++ src/predunder/hypertuning.py
@@ -24,6 +24,8 @@ def tune_dnn(train: PandasDataFrame, label: str, folds: int, max_nodes: int, ove
         [label], axis=1).columns
 
     for i in range(1, max_nodes+1):
+        for _ in range(5):
+            print()
         print("Training", [i])
         metrics = train_kfold(train, label, folds, train_dnn, features=features, layers=[i], oversample=oversample)
         rowdf = kfold_metrics_to_df(metrics)
@@ -32,6 +34,8 @@ def tune_dnn(train: PandasDataFrame, label: str, folds: int, max_nodes: int, ove
 
     for i in range(1, max_nodes+1):
         for j in range(1, max_nodes+1):
+            for _ in range(5):
+                print()
             print("Training", [i, j])
             metrics = train_kfold(train, label, folds, train_dnn, features=features, layers=[i, j], oversample=oversample)
             rowdf = kfold_metrics_to_df(metrics)
